2c577b8736ea63b21a8cbdb6ac7dc18dc8feddb9
Zheng Kai <zhengkai@gmail.com>
2021-06-16 18:59:33 +0800

    head

--- client/src/app/common/bootstrap.component.ts
+++ client/src/app/common/bootstrap.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { environment }  from '../../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './bootstrap.component.html',
-  styleUrls: ['./bootstrap.component.scss']
+	selector: 'app-root',
+	templateUrl: './bootstrap.component.html',
+	styleUrls: ['./bootstrap.component.scss']
 })
 export class BootstrapComponent {
-  title = 'Tank';
+	title = 'Tank';
+	prod = false;
+
+	constructor() {
+		this.prod = environment.production;
+	}
 }
