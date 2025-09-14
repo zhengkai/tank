ba6aca26406010bacfbbbfe3797beac679ec8715
Zheng Kai <zhengkai@gmail.com>
2025-09-14 17:12:06 +0800

    task script

--- task/crawl.sh
+++ task/crawl.sh
@@ -1,16 +1,3 @@
 #!/bin/bash -ex
 
-curl localhost:21024/task/crawl
-
-echo
-cd /www/tank/static/data || exit 1
-
-find . -type f \( -name '*.gz' -o -name '*.br' \) -delete
-find . -type f \( -name '*.json' -o -name '*.pb' \) -size +1k -exec sh -c '
-  for f; do
-    gzip -c "$f" > "$f.gz"
-    brotli -q 11 -o "$f.br" "$f"
-  done
-' sh {} +
-
-rsync --partial -vzrtopg ./ doll:/www/tank/data
+curl -s --max-time 10 --noproxy --ipv4 localhost:21024/task/crawl
